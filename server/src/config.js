07eaa65511d5793579bd35da198f6e0dc13d53e9
sosa <120314312+shest-ernev@users.noreply.github.com>
2024-06-01 19:57:16 +0300

    Update config.js

--- server/src/config.js
+++ server/src/config.js
@@ -1,5 +1,5 @@
 export const config = {
-   url: 'https://localhost:8001',
+   url: 'http://localhost:8001',
    port: 8001,
    db: 'mongodb+srv://sosa:sosa2004@sosa.bh1ursi.mongodb.net/sosa' // Измените на свою базу если эта не работает
 }
